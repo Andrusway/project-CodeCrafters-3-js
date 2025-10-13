b6e04618b6488aed7d52cad0c1c9e5d003ecc96a
Andrii_Petrukh <andriypetruch2004@gmail.com>
2025-10-13 21:07:04 +0300

    Fix merge conflict in refs.js

--- src/js/refs.js
+++ src/js/refs.js
@@ -4,10 +4,7 @@ export const navbarBtn = document.querySelector('.navbar-btn');
 export const navbarCloseBtn = document.querySelector('.navbar-close-btn');
 export const navbarList = document.querySelector('ul.navbar');
 export const mediaQuery = window.matchMedia('(max-width:767px)');
-<<<<<<< HEAD
 
-=======
->>>>>>> main
 
 export const refs = {
   backdrop: document.querySelector('.backdrop'),
