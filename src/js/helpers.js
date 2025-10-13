6609e6a3ec8e926cb6849caf127ef1fe8ef3796d
Andrii_Petrukh <andriypetruch2004@gmail.com>
2025-10-13 21:15:25 +0300

    Fix syntax and merge conflict in helpers.js

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -1,56 +1,29 @@
-<<<<<<< HEAD
 import { handleNavigation } from './handlers';
 import { navbarList, body } from './refs';
+import iziToast from 'izitoast';
 
 // ф-я блокування скролу на боді - додає клас стилів
 export function stopScroll() {
   body.classList.toggle('no-scroll');
 }
 
-=======
-import { handleNavigation } from "./handlers";
-import { navbarList, body, refs } from './refs';
-import iziToast from 'izitoast';
-
-// ф-я блокування скролу на боді - додає клас стилів
-export function stopScroll() {
-    body.classList.toggle("no-scroll")
-
-}
-
-
->>>>>>> main
 // Функції HandleScreenChange та OnNavClick зроблені для того,
 // щоб перехід по секціям в Desktop-і та Tablet-версії відбувався по якорним посиланням,
 // а в мобільній версії відпрацьовула функція HandleNavigation.
 
-// ф-я перевіряє розмір екрану, якщо він відповідає умові(max-width:767px)
-// то ставить слухача події на навбар в іншому разі ні
-<<<<<<< HEAD
+// ф-я перевіряє розмір екрану, якщо він відповідає умові (max-width:767px)
+// то ставить слухача події на навбар, в іншому разі — знімає
 export function handleScreenChange(e) {
   if (e.matches) {
     navbarList.addEventListener('click', onNavClick);
   } else {
     navbarList.removeEventListener('click', onNavClick);
   }
-=======
-export function handleScreenChange(e){
-
-    if (e.matches) {
-        navbarList.addEventListener("click", onNavClick)
-    } else {
-        navbarList.removeEventListener("click", onNavClick)
-    }
->>>>>>> main
 }
 
-// викликає ф-ю навігації моб версії
+// викликає ф-ю навігації мобільної версії
 export function onNavClick(event) {
-<<<<<<< HEAD
   handleNavigation(event);
-}
-=======
-    handleNavigation(event);
 }
 
 export function toTitleCase(str) {
@@ -72,6 +45,5 @@ export function showSuccessToast(message) {
 }
 
 export function getBooksSelected() {
-  return  Number(refs.bookModalCountInput.value);
+  return Number(refs.bookModalCountInput.value);
 }
->>>>>>> main
